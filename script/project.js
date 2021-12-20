3058e8ddc8cbe9f09704ca301db77050e496037a
Anindra Das Bivas <bivasdas911@gmail.com>
2021-12-20 15:17:23 +0600

    Updated the site with new projects and skills

--- script/project.js
+++ script/project.js
@@ -3,12 +3,21 @@ const projects_link = () => {
   const sorting_visualizer_project = document.getElementById(
     "sorting-algo-visualizer"
   );
-
+  const password_manager_project = document.getElementById("Password-Manager");
+  const project_management_app = document.getElementById(
+    "Project-Management-App"
+  );
   web_portfolio_project.addEventListener("click", () => {
     window.open("https://github.com/Anindra123/portfolio");
   });
   sorting_visualizer_project.addEventListener("click", () => {
     window.open("https://github.com/Anindra123/SortingAlgorithmVisualizer");
+  });
+  password_manager_project.addEventListener("click", () => {
+    window.open("https://github.com/Anindra123/PasswordManagerCli");
+  });
+  project_management_app.addEventListener("click", () => {
+    window.open("https://github.com/Anindra123/ProjectManagementApp");
   });
 };
 
